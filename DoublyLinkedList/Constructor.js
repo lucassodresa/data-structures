07d9e82a4f2989526a4593d95074474e81c2c225
lucassodresa <lucas.sodresa@gmail.com>
2022-12-28 16:54:06 +0000

    DLL: Constructor

---
+++ DoublyLinkedList/Constructor.js
@@ -0,0 +1,83 @@
+// WRITE NODE CLASS HERE //
+//                       //
+//                       //
+//                       //
+//                       //
+///////////////////////////
+
+class Node {
+  constructor(value) {
+    this.prev = null;
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class DoublyLinkedList {
+  // WRITE DLL CONSTRUCTOR HERE //
+  //                            //
+  //                            //
+  //                            //
+  //                            //
+  ////////////////////////////////
+
+  constructor(value) {
+    const newNode = new Node(value);
+    this.head = this.tail = newNode;
+    this.length = 1;
+  }
+
+  printList() {
+    let temp = this.head;
+    while (temp !== null) {
+      console.log(temp.value);
+      temp = temp.next;
+    }
+  }
+
+  getHead() {
+    if (this.head === null) {
+      console.log("Head: null");
+    } else {
+      console.log("Head: " + this.head.value);
+    }
+  }
+
+  getTail() {
+    if (this.tail === null) {
+      console.log("Tail: null");
+    } else {
+      console.log("Tail: " + this.tail.value);
+    }
+  }
+
+  getLength() {
+    console.log("Length: " + this.length);
+  }
+}
+
+// DO NOT CHANGE TEST FUNCTION
+function test() {
+  let myDLL = new DoublyLinkedList(7);
+
+  myDLL.getHead();
+  myDLL.getTail();
+  myDLL.getLength();
+
+  console.log("\nDoubly Linked List:");
+  myDLL.printList();
+}
+
+test();
+
+/*
+    EXPECTED OUTPUT:
+    ----------------
+    Head: 7
+    Tail: 7
+    Length: 1
+
+    Doubly Linked List:
+    7
+
+*/
